497dba35c0372e3d9064776ff9f30b302a6f19a8
Benjamin Reed <rangerrick@befunk.com>
2014-03-14 17:23:22 -0400

    create one context per test run

--- sampler-config-snmp/src/test/java/org/opennms/netmgt/sampler/config/snmp/SnmpConfigRoutesTest.java
+++ sampler-config-snmp/src/test/java/org/opennms/netmgt/sampler/config/snmp/SnmpConfigRoutesTest.java
@@ -6,7 +6,6 @@ import java.net.URL;
 import java.util.Dictionary;
 
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opennms.netmgt.api.sample.support.SingletonBeanFactory;
@@ -18,89 +17,94 @@ import ch.qos.logback.classic.LoggerContext;
 
 public class SnmpConfigRoutesTest extends CamelBlueprintTestSupport {
 
-	private static final String OPENNMS_HOME = "src/test/resources";
+    private static final String OPENNMS_HOME = "src/test/resources";
 
-	private static URL url(String path) throws MalformedURLException {
-		return new URL("file:" + OPENNMS_HOME + "/" + path);
-	}
+    private static URL url(String path) throws MalformedURLException {
+        return new URL("file:" + OPENNMS_HOME + "/" + path);
+    }
 
-	@BeforeClass
-	public static void configureLogging() throws SecurityException, IOException {
-		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-		lc.getLogger("org.apache.aries.blueprint").setLevel(Level.INFO);
-	}
+    @BeforeClass
+    public static void configureLogging() throws SecurityException, IOException {
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        lc.getLogger("org.apache.aries.blueprint").setLevel(Level.INFO);
+    }
 
-	@Override
-	public boolean isUseAdviceWith() {
-		return true;
-	}
+    @Override
+    public boolean isCreateCamelContextPerClass() {
+        return true;
+    }
 
-	@Override
-	public boolean isUseDebugger() {
-		// must enable debugger
-		return true;
-	}
+    @Override
+    public boolean isUseAdviceWith() {
+        return true;
+    }
 
-	// The location of our Blueprint XML file to be used for testing
-	@Override
-	protected String getBlueprintDescriptor() {
-		return "file:src/main/resources/OSGI-INF/blueprint/blueprint.xml";
-	}
+    @Override
+    public boolean isUseDebugger() {
+        // must enable debugger
+        return true;
+    }
 
-	/**
-	 * Override 'opennms.home' with the test resource directory.
-	 */
-	@Override
-	protected String useOverridePropertiesWithConfigAdmin(Dictionary props) throws Exception {
-		props.put("opennms.home", OPENNMS_HOME);
-		return "org.opennms.netmgt.sampler.config.snmp";
-	}
+    // The location of our Blueprint XML file to be used for testing
+    @Override
+    protected String getBlueprintDescriptor() {
+        return "file:src/main/resources/OSGI-INF/blueprint/blueprint.xml";
+    }
 
-	@Test
-	public void testParseXML() throws Exception {
-	        System.err.printf("Starting testParseXML");
-		SnmpConfiguration resultsUsingURL = template.requestBody("direct:parseXML", new URL("file:" + OPENNMS_HOME + "/etc/snmp-config.xml"), SnmpConfiguration.class);
+    /**
+     * Override 'opennms.home' with the test resource directory.
+     */
+    @Override
+    protected String useOverridePropertiesWithConfigAdmin(Dictionary props) throws Exception {
+        props.put("opennms.home", OPENNMS_HOME);
+        return "org.opennms.netmgt.sampler.config.snmp";
+    }
 
-		System.err.printf("Results Using URL: %s\n", resultsUsingURL);
-		assertNotNull(resultsUsingURL);
+    @Test
+    public void testParseXML() throws Exception {
+        System.err.printf("Starting testParseXML");
+        SnmpConfiguration resultsUsingURL = template.requestBody("direct:parseXML", new URL("file:" + OPENNMS_HOME + "/etc/snmp-config.xml"), SnmpConfiguration.class);
 
-		SnmpConfiguration resultsUsingString = template.requestBody("direct:parseXML", "file:" + OPENNMS_HOME + "/etc/snmp-config.xml", SnmpConfiguration.class);
+        System.err.printf("Results Using URL: %s\n", resultsUsingURL);
+        assertNotNull(resultsUsingURL);
 
-		System.err.printf("Results Using String: %s\n", resultsUsingString);
-		assertNotNull(resultsUsingString);
-	}
+        SnmpConfiguration resultsUsingString = template.requestBody("direct:parseXML", "file:" + OPENNMS_HOME + "/etc/snmp-config.xml", SnmpConfiguration.class);
 
-	@Test
-	public void testLoadSnmpConfig() throws Exception {
-            System.err.printf("Starting testLoadSnmpConfig");
+        System.err.printf("Results Using String: %s\n", resultsUsingString);
+        assertNotNull(resultsUsingString);
+    }
 
-            template.requestBody("direct:loadSnmpConfig", null, String.class);
+    @Test
+    public void testLoadSnmpConfig() throws Exception {
+        System.err.printf("Starting testLoadSnmpConfig");
 
-		SingletonBeanFactory<SnmpConfiguration> configSvc = bean("snmpConfiguration", SingletonBeanFactory.class);
+        template.requestBody("direct:loadSnmpConfig", null, String.class);
 
-		System.err.printf("configSvc: %s\n", configSvc);
-		assertNotNull(configSvc);
-		assertNotNull(configSvc.getInstance());
+        SingletonBeanFactory<SnmpConfiguration> configSvc = bean("snmpConfiguration", SingletonBeanFactory.class);
 
-	}
+        System.err.printf("configSvc: %s\n", configSvc);
+        assertNotNull(configSvc);
+        assertNotNull(configSvc.getInstance());
 
-	/**
-	 * Test loading the {@link SnmpMetricRepository}.
-	 */
-	@Test
-	public void testLoadDataCollectionConfig() throws Exception {
-	    System.err.printf("Starting testLoadDataCollectionConfig");
-		template.requestBody("direct:loadDataCollectionConfig", null, String.class);
+    }
 
-		SnmpMetricRepository metricRepo = bean("snmpMetricRepository", SnmpMetricRepository.class);
+    /**
+     * Test loading the {@link SnmpMetricRepository}.
+     */
+    @Test
+    public void testLoadDataCollectionConfig() throws Exception {
+        System.err.printf("Starting testLoadDataCollectionConfig");
+        template.requestBody("direct:loadDataCollectionConfig", null, String.class);
 
-	            System.err.printf("metricRepo: %s\n", metricRepo);
-		assertNotNull(metricRepo);
-		assertNotNull(metricRepo.getMetric("ifInOctets"));
+        SnmpMetricRepository metricRepo = bean("snmpMetricRepository", SnmpMetricRepository.class);
 
-	}
+        System.err.printf("metricRepo: %s\n", metricRepo);
+        assertNotNull(metricRepo);
+        assertNotNull(metricRepo.getMetric("ifInOctets"));
 
-	private <T> T bean(String name,	Class<T> type) {
-		return context().getRegistry().lookupByNameAndType(name, type);
-	}
+    }
+
+    private <T> T bean(String name,	Class<T> type) {
+        return context().getRegistry().lookupByNameAndType(name, type);
+    }
 }
