d8650afe6804c9969e9cc9888bd1f5ae873eea05
Seth Leger <seth@opennms.org>
2014-04-01 12:38:43 -0400

    Added unit tests for sampler-routes blueprint files. Added IDs to all CamelContext objects created in blueprints.

---
+++ sampler-routes/src/test/java/org/opennms/netmgt/sampler/routes/ActiveMQStorageContextTest.java
@@ -0,0 +1,74 @@
+package org.opennms.netmgt.sampler.routes;
+
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Map;
+
+import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.apache.camel.util.KeyValueHolder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opennms.netmgt.api.sample.SampleRepository;
+import org.opennms.netmgt.api.sample.SampleSetDispatcher;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+
+public class ActiveMQStorageContextTest extends CamelBlueprintTestSupport {
+	
+	/**
+	 * Use Aries Blueprint synchronous mode to avoid a blueprint
+	 * deadlock bug.
+	 * 
+	 * @see https://issues.apache.org/jira/browse/ARIES-1051
+	 * @see https://access.redhat.com/site/solutions/640943
+	 */
+	@Override
+	public void doPreSetup() throws Exception { 
+		System.setProperty("org.apache.aries.blueprint.synchronous", Boolean.TRUE.toString());
+		System.setProperty("de.kalpatec.pojosr.framework.events.sync", Boolean.TRUE.toString());
+	}
+
+	@BeforeClass
+	public static void configureLogging() throws SecurityException, IOException {
+		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+		lc.getLogger("org.apache.aries.blueprint").setLevel(Level.INFO);
+	}
+
+	@Override
+	public boolean isUseAdviceWith() {
+		return true;
+	}
+
+	@Override
+	public boolean isUseDebugger() {
+		// must enable debugger
+		return true;
+	}
+
+	// The location of our Blueprint XML file to be used for testing
+	@Override
+	protected String getBlueprintDescriptor() {
+		return "file:routes-persister.xml";
+	}
+
+	/**
+	 * Register a mock OSGi {@link SampleRepository}.
+	 */
+	@SuppressWarnings("rawtypes")
+	@Override
+	protected void addServicesOnStartup(Map<String, KeyValueHolder<Object, Dictionary>> services) {
+		try {
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test(timeout=60000)
+	public void test() throws Exception {
+
+		assertTrue(getOsgiService(SampleSetDispatcher.class) != null);
+
+	}
+}
