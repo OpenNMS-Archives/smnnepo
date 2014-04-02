b0ae8e9f07922610507b770ca43c1f1ab57d98f1
Seth Leger <seth@opennms.org>
2014-04-02 12:06:19 -0400

    Added actual calls to the ActiveMQ route so that the broker is created and messages are routed.

--- sampler-routes/src/test/java/org/opennms/netmgt/sampler/routes/ActiveMQStorageContextTest.java
+++ sampler-routes/src/test/java/org/opennms/netmgt/sampler/routes/ActiveMQStorageContextTest.java
@@ -1,22 +1,29 @@
 package org.opennms.netmgt.sampler.routes;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Dictionary;
 import java.util.Map;
 
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.RouteDefinition;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 import org.apache.camel.util.KeyValueHolder;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opennms.netmgt.api.sample.SampleRepository;
+import org.opennms.netmgt.api.sample.SampleSet;
 import org.opennms.netmgt.api.sample.SampleSetDispatcher;
+import org.opennms.netmgt.api.sample.Timestamp;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 
 public class ActiveMQStorageContextTest extends CamelBlueprintTestSupport {
-	
+
 	/**
 	 * Use Aries Blueprint synchronous mode to avoid a blueprint
 	 * deadlock bug.
@@ -30,10 +37,15 @@ public class ActiveMQStorageContextTest extends CamelBlueprintTestSupport {
 		System.setProperty("de.kalpatec.pojosr.framework.events.sync", Boolean.TRUE.toString());
 	}
 
+	/**
+	 * TODO: This isn't working properly because the ActiveMQ logs aren't getting routed
+	 * through logback somehow.
+	 */
 	@BeforeClass
 	public static void configureLogging() throws SecurityException, IOException {
 		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
 		lc.getLogger("org.apache.aries.blueprint").setLevel(Level.INFO);
+		lc.getLogger("org.apache.activemq.broker.jmx").setLevel(Level.INFO);
 	}
 
 	@Override
@@ -67,8 +79,29 @@ public class ActiveMQStorageContextTest extends CamelBlueprintTestSupport {
 
 	@Test(timeout=60000)
 	public void test() throws Exception {
+		// Add mock endpoints to the route context
+		for (RouteDefinition route : new ArrayList<RouteDefinition>(context.getRouteDefinitions())) {
+			route.adviceWith(context, new AdviceWithRouteBuilder() {
+				@Override
+				public void configure() throws Exception {
+					mockEndpoints();
+				}
+			});
+		}
+		context.start();
 
-		assertTrue(getOsgiService(SampleSetDispatcher.class) != null);
+		assertTrue(context.hasEndpoint("mock:activemq:sampleSet") != null);
+		MockEndpoint endpoint = getMockEndpoint("mock:activemq:sampleSet", false);
+		endpoint.setExpectedMessageCount(2);
 
+		SampleSetDispatcher dispatcher = getOsgiService(SampleSetDispatcher.class);
+		assertTrue(dispatcher != null);
+		
+		SampleSet set = new SampleSet(new Timestamp(new Date()));
+		dispatcher.save(set);
+		set = new SampleSet(new Timestamp(new Date()));
+		dispatcher.save(set);
+
+		assertMockEndpointsSatisfied();
 	}
 }
