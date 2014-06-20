0d9705c63c02d16c3e9d46f05add84cdb798918f
Benjamin Reed <rangerrick@befunk.com>
2014-06-20 16:31:18 -0400

    Updates to the minion code to make the XML more robust

---
+++ minion-controller-api/src/main/java/org/opennms/minion/controller/api/DateAdapter.java
@@ -0,0 +1,29 @@
+package org.opennms.minion.controller.api;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class DateAdapter extends XmlAdapter<String, Date> {
+    private final ThreadLocal<DateFormat> m_dateFormat = new ThreadLocal<DateFormat>() {
+        protected DateFormat initialValue() {
+            final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.ENGLISH);
+            dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            return dateFormat;
+        }
+    };
+
+    @Override
+    public Date unmarshal(final String v) throws Exception {
+        return m_dateFormat.get().parse(v);
+    }
+
+    @Override
+    public String marshal(final Date v) throws Exception {
+        return m_dateFormat.get().format(v);
+    }
+}
