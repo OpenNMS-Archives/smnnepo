7bdb8969bbd2da3bd2495924e9ce3f6e1e35a071
Seth Leger <seth@opennms.org>
2014-07-10 14:48:56 -0400

    Changed default Karaf SSH ports to start at 8201 to avoid conflicts with standalone Karaf instances and OpenNMS. Changed the init scripts so that they call smnnepo-setup.karaf to configure the root instance and create clones for each subinstance, then call smnnepo-setup.karaf in each subinstance to configure and install the desired features for that instance.

--- smnnepo.sh
+++ smnnepo.sh
@@ -48,26 +48,63 @@ fi
 "$SMNNEPO_HOME"/bin/start >/dev/null
 RETVAL="$?"
 if [ $RETVAL -eq 0 ]; then
-        # FIXME We can't start until karaf is ready to run the script need a script that will poll for readiness
-	sleep 60
+	echo "OK"
+
 	OPENNMS=`echo "$OPENNMS" | sed -e 's,/$,,'`
-	if [ -n "$BROKER" ]; then
-		echo -n "configuring container: URL=$OPENNMS, Location=$LOCATION: "
-		"$SMNNEPO_HOME"/bin/client "source" "\"$OPENNMS/smnnepo/smnnepo-setup.karaf\"" "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$BROKER\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
+	SCRIPTDIR=$OPENNMS/smnnepo
+	#SCRIPTDIR=/root/git/smnnepo/sampler-repo-webapp/src/main/webapp
+
+	# Make sure that the port here matches the default port from the smnnepo.spec
+	if [ -z "$BROKER" ]; then
+		echo -n "Configuring root instance: URL: $OPENNMS, Location: $LOCATION: "
+		"$SMNNEPO_HOME"/bin/client -r 30 -a 8201 "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" root "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
 		RETVAL="$?"
 	else
-		echo -n "configuring container: URL=$OPENNMS, Broker=$BROKER, Location=$LOCATION: "
-		"$SMNNEPO_HOME"/bin/client "source" "\"$OPENNMS/smnnepo/smnnepo-setup.karaf\"" "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
+		echo -n "Configuring root instance: URL: $OPENNMS, Broker: $BROKER, Location: $LOCATION: "
+		"$SMNNEPO_HOME"/bin/client -r 30 -a 8201 "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" root "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$BROKER\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
 		RETVAL="$?"
 	fi
 
 	if [ $RETVAL -eq 0 ]; then
 		echo "OK"
-		exit 0
 	else
-		echo "FAILED, see /tmp/smnnepo.log for details"
+		echo "FAILED. See /tmp/smnnepo.log for details."
 		exit $RETVAL
 	fi
+
+	# List all of the instances and for each instance, call the smnnepo-setup.karaf script
+	#INSTANCES=`"$SMNNEPO_HOME"/bin/admin list | grep '^\[' | sed 's#^\[\s*\([0-9]*\)\]\s*\[\s*\([0-9]*\)/\([0-9]*\)\s*\]\s*\[\s*\([[:alnum:]]*\)\s*\]\s*\[\([0-9]*\)\s*\]\s*\(.*\)$#\1 \2 \3 \4 \5 \6#'`
+
+	# Filter out all of the [, ], and / characters
+	"$SMNNEPO_HOME"/bin/admin list | grep '^\[' | sed 's#[][/]# #g' | while read LINE; do
+		echo "$LINE" >/tmp/smnnepo.log 2>&1
+		PORT=`echo "$LINE" | awk '{print $1}'`
+		INSTANCE=`echo "$LINE" | awk '{print $6}'`
+
+		sleep 10
+
+		# Configure all instances except root
+		if [ "$INSTANCE" != "root" ]; then
+			echo -n "Configuring subinstance $INSTANCE: "
+			if [ -z "$BROKER" ]; then
+				"$SMNNEPO_HOME"/bin/client -r 10 -a $PORT "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" $INSTANCE "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
+				RETVAL="$?"
+			else
+				"$SMNNEPO_HOME"/bin/client -r 10 -a $PORT "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" $INSTANCE "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$BROKER\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
+				RETVAL="$?"
+			fi
+
+			if [ $RETVAL -eq 0 ]; then
+				echo "OK"
+			else
+				echo "FAILED. See /tmp/smnnepo.log for details."
+				exit $RETVAL
+			fi
+		fi
+	done
+
+	exit 0
+
 else
 	echo "FAILED"
 	exit $RETVAL
