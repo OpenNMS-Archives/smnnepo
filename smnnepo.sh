7a54ed902a793f377cffe31e54a0a55c5acf498f
Benjamin Reed <rangerrick@befunk.com>
2014-08-28 15:54:08 -0400

    simplify init since it will be up to the minion to set things up

--- smnnepo.sh
+++ smnnepo.sh
@@ -56,11 +56,11 @@ if [ $RETVAL -eq 0 ]; then
 	# Make sure that the port here matches the default port from the smnnepo.spec
 	if [ -z "$BROKER" ]; then
 		echo -n "Configuring root instance: URL: $OPENNMS, Location: $LOCATION: "
-		"$SMNNEPO_HOME"/bin/client -r 30 -a 8201 "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" root "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
+		"$SMNNEPO_HOME"/bin/client -r 30 -a 8201 "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
 		RETVAL="$?"
 	else
 		echo -n "Configuring root instance: URL: $OPENNMS, Broker: $BROKER, Location: $LOCATION: "
-		"$SMNNEPO_HOME"/bin/client -r 30 -a 8201 "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" root "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$BROKER\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
+		"$SMNNEPO_HOME"/bin/client -r 30 -a 8201 "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$BROKER\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
 		RETVAL="$?"
 	fi
 
@@ -70,36 +70,6 @@ if [ $RETVAL -eq 0 ]; then
 		echo "FAILED. See /tmp/smnnepo.log for details."
 		exit $RETVAL
 	fi
-
-	# Sleep to allow the subinstances to start up so we don't run into "command not found" errors
-	sleep 10
-
-	# List all of the instances and for each instance, call the smnnepo-setup.karaf script
-	# Filter out all of the [, ], and / characters from the admin:list output
-	"$SMNNEPO_HOME"/bin/admin list | grep '^\[' | sed 's#[][/]# #g' | while read LINE; do
-
-		PORT=`echo "$LINE" | awk '{print $1}'`
-		INSTANCE=`echo "$LINE" | awk '{print $6}'`
-
-		# Configure all instances except root
-		if [ "$INSTANCE" != "root" ]; then
-			echo -n "Configuring subinstance $INSTANCE: "
-			if [ -z "$BROKER" ]; then
-				"$SMNNEPO_HOME"/bin/client -r 10 -a $PORT "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" $INSTANCE "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
-				RETVAL="$?"
-			else
-				"$SMNNEPO_HOME"/bin/client -r 10 -a $PORT "source" "\"$SCRIPTDIR/smnnepo-setup.karaf\"" $INSTANCE "\"$USERNAME\"" "\"$PASSWORD\"" "\"$OPENNMS\"" "\"$BROKER\"" "\"$LOCATION\"" >/tmp/smnnepo.log 2>&1
-				RETVAL="$?"
-			fi
-
-			if [ $RETVAL -eq 0 ]; then
-				echo "OK"
-			else
-				echo "FAILED. See /tmp/smnnepo.log for details."
-				exit $RETVAL
-			fi
-		fi
-	done
 
 	exit 0
 
