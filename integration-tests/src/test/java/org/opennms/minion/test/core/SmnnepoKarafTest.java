8492ca6db1a02b9b44017429b5c7d402f4e14c1b
Seth Leger <seth@opennms.org>
2015-04-27 18:04:05 -0400

    Upgraded PAX Exam to try and fix bug PJSM-244 but... no dice.

--- integration-tests/src/test/java/org/opennms/minion/test/core/SmnnepoKarafTest.java
+++ integration-tests/src/test/java/org/opennms/minion/test/core/SmnnepoKarafTest.java
@@ -6,7 +6,7 @@ import static org.ops4j.pax.exam.CoreOptions.maven;
 
 public abstract class SmnnepoKarafTest extends KarafTestCase {
     @Before
-    public void registerOpenNMSFeatuers() {
+    public void registerMinionFeatures() {
         // TODO remove constant version and use versionAsInProject instead (see PJSM-244)
         addFeaturesUrl(maven().groupId("org.opennms.netmgt.sample").artifactId("karaf").version("16.0.0-SNAPSHOT").type("xml").getURL());
     }
