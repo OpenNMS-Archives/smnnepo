0598e4d4b872686a42d2b4a2db9a872b63c668c6
Seth Leger <seth@opennms.org>
2015-10-08 14:50:10 -0400

    Added ActiveMQ broker with short timeout to MinionControllerFeatureInstallTest so that it completes with in-flight messages still present.

--- integration-tests/src/test/java/org/opennms/minion/test/MinionControllerFeatureInstallTest.java
+++ integration-tests/src/test/java/org/opennms/minion/test/MinionControllerFeatureInstallTest.java
@@ -1,10 +1,12 @@
 package org.opennms.minion.test;
 
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
 
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.camel.CamelContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.minion.test.core.SmnnepoKarafTest;
@@ -27,12 +29,21 @@ public class MinionControllerFeatureInstallTest extends SmnnepoKarafTest {
     public Option[] config() {
         List<Option> optionsList = super.configAsList();
         optionsList.add(editConfigurationFilePut("etc/org.opennms.minion.controller.cfg", "location", "dummy"));
+        // Add the Camel bundle to the container so we can use it in this test
+        optionsList.add(mavenBundle().groupId("org.apache.camel").artifactId("camel-core").version("2.14.1"));
         return optionsList.toArray(new Option[0]);
     }
 
     @Test
     public void testMinionController() throws IOException, InvalidSyntaxException {
+        // Start an ActiveMQ broker
+        installFeature("opennms-activemq");
+        // Install the controller feature
         installFeature("minion-controller");
+
+        // Change the Camel shutdown timeout since we're leaving messages in-flight
+        CamelContext context = getOsgiService(CamelContext.class);
+        context.getShutdownStrategy().setTimeout(3);
     }
 
 }
